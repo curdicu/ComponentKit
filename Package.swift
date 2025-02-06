65840b52bafcda0d57480e9cc2604c831625a4dd
curdicu <curdicu@gmail.com>
2025-02-07 01:30:03 +0800

    *

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         dependencies: [
             .package(url: "https://github.com/Juanpe/SkeletonView.git", from: "1.7.0"),
             .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
-            .package(url: "https://github.com/horizontalsystems/HUD.Swift.git", .upToNextMajor(from: "2.0.0")),
+            .package(url: "https://github.com/curdicu/HUD.git", .branch("main")),
             .package(url: "https://github.com/horizontalsystems/SectionsTableView.Swift.git", .upToNextMajor(from: "1.0.0")),
             .package(url: "https://github.com/curdicu/ThemeKit.git" , .branch("main")),
             .package(url: "https://github.com/horizontalsystems/UIExtensions.Swift.git", .upToNextMajor(from: "1.0.0")),
@@ -26,7 +26,7 @@ let package = Package(
                     dependencies: [
                         "SkeletonView",
                         "SnapKit",
-                        .product(name: "HUD", package: "HUD.Swift"),
+                        .product(name: "HUD", package: "HUD"),
                         .product(name: "SectionsTableView", package: "SectionsTableView.Swift"),
                         .product(name: "ThemeKit", package: "ThemeKit"),
                         .product(name: "UIExtensions", package: "UIExtensions.Swift"),
