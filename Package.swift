20221f2dcae26326d680474c21c939b310895cdf
curdicu <curdicu@gmail.com>
2025-02-23 00:03:14 +0800

    *

--- Package.swift
+++ Package.swift
@@ -16,9 +16,9 @@ let package = Package(
             .package(url: "https://github.com/Juanpe/SkeletonView.git", from: "1.7.0"),
             .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
             .package(url: "https://github.com/curdicu/HUD.git", .branch("main")),
-            .package(url: "https://github.com/horizontalsystems/SectionsTableView.Swift.git", .upToNextMajor(from: "1.0.0")),
+            .package(url: "https://github.com/curdicu/SectionsTableView.git", .branch("main")),
             .package(url: "https://github.com/curdicu/ThemeKit.git" , .branch("main")),
-            .package(url: "https://github.com/horizontalsystems/UIExtensions.Swift.git", .upToNextMajor(from: "1.0.0")),
+            .package(url: "https://github.com/curdicu/UIExtensions.git", .branch( "main")),
         ],
         targets: [
             .target(
@@ -27,9 +27,9 @@ let package = Package(
                         "SkeletonView",
                         "SnapKit",
                         .product(name: "HUD", package: "HUD"),
-                        .product(name: "SectionsTableView", package: "SectionsTableView.Swift"),
+                        .product(name: "SectionsTableView", package: "SectionsTableView"),
                         .product(name: "ThemeKit", package: "ThemeKit"),
-                        .product(name: "UIExtensions", package: "UIExtensions.Swift"),
+                        .product(name: "UIExtensions", package: "UIExtensions"),
                     ]
             ),
         ]
